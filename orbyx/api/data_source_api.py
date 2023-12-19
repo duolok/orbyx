e5612a1b4e06f850fdb0ed840508e303b54fcbcb
dusanlecic <dusan.lecic5@gmail.com>
2023-12-19 22:51:27 +0100

    add: comments for abstract methods

--- orbyx/api/data_source_api.py
+++ orbyx/api/data_source_api.py
@@ -4,8 +4,10 @@ from typing import Any, Dict, List
 class DataSourceAPI(ABC):
     @abstractmethod
     def parse_data(self, data: Any) -> List[Dict[str, Any]]:
+        """Method that will parse data and consturct a graph"""
         pass
 
     @abstractmethod
     def get_data(self) -> Graph:
+        """Method that will send data to the plugin and recieve a graph"""
         pass
--- orbyx/api/visualizer_api.py
+++ orbyx/api/visualizer_api.py
@@ -4,4 +4,5 @@ from typing import Any
 class Visualizer(ABC):
     @abstractmethod
     def visualize(self, data: Any) -> Any:
+        """Abstract method for data visualization"""
         pass
