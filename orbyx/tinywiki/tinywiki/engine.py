4dcc45c9bba43e873849b2616f21f2f82e8bd8bc
dusanlecic <dusan.lecic5@gmail.com>
2024-02-03 20:59:52 +0100

    #10 update: graph parsing

--- orbyx/tinywiki/tinywiki/engine.py
+++ orbyx/tinywiki/tinywiki/engine.py
@@ -3,7 +3,8 @@ import sys
 
 from model.graph.graph import Graph
 from services.data_source_api import DataSourceAPI
-from scraper import *
+from tinywiki.scraper import get_scraped_dictionary
+from typing import Any, List, Dict
 
 class WikipediaDataSource(DataSourceAPI):
     def parse_data(self, data: Any) -> List[Dict[str, Any]]:
@@ -14,31 +15,34 @@ class WikipediaDataSource(DataSourceAPI):
 
     def get_graph(self, parsed_data: List[Dict[str, Any]]) -> Graph:
         graph = Graph()
-        data_dict = {list(page_data.keys())[0]: list(page_data.values())[0] for page_data in parsed_data}
+        vertex_map = {}
 
-        for url, page_data in data_dict.items():
-            name = page_data["name"]
-            paragraphs = page_data["paragraphs"]
-            graph.insert_node({"name": name, "paragraphs": paragraphs})
+        for page_data in parsed_data:
+            url = list(page_data.keys())[0]
+            page_info = list(page_data.values())[0]
+            name = page_info["name"]
+            paragraphs = page_info["paragraphs"]
+            children = page_info["children"]
 
-            for child_url in page_data["children"]:
-                if child_url in data_dict:  
-                    child_name = data_dict[child_url]["name"]
-                    graph.insert_edge(name, child_name)
+            node_value = {
+                "url": url,
+                "name": name,
+                "paragraphs": paragraphs,
+                "children": len(children)
+            }
+
+            node = GraphNode(node_value)
+            vertex = graph.insert_node(node)
+            vertex_map[url] = vertex
+
+        for page_data in parsed_data:
+            url = list(page_data.keys())[0]
+            page_info = list(page_data.values())[0]
+
+            for child_url in page_info["children"]:
+                parent_vertex = vertex_map[url]
+                child_vertex = vertex_map[child_url]
+                if graph.get_edge(parent_vertex, child_vertex) is None:
+                    graph.insert_edge(parent_vertex, child_vertex)
+
         return graph
-
-def main():
-    wikipedia_data_source = WikipediaDataSource()
-    parsed_data = wikipedia_data_source.parse_data(None)  
-    graph = wikipedia_data_source.get_graph(parsed_data)
-
-    print("Graph Nodes:")
-    for node in graph.get_nodes():
-        print(f"Node: {node['name']}, Paragraphs: {len(node['paragraphs'])}")
-
-    print("\nGraph Edges:")
-    for edge in graph.get_edges():
-        print(f"Edge: {edge}")
-
-if __name__ == "__main__":
-    main()
