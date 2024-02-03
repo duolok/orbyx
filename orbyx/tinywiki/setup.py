565c04462fedd04c55f0409d0b443072be1e4778
dusanlecic <dusan.lecic5@gmail.com>
2024-02-03 19:11:57 +0100

    #10 add: plugin setup

---
+++ orbyx/tinywiki/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name="wikipedia_data_source",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'orbyx_data_source_plugin':
+            ['template=tinywiki.engine:WikipediaDataSource'],
+    },
+    install_requires=[
+        'orbyx-api==0.1',
+    ],
+    zip_safe=True
+)
