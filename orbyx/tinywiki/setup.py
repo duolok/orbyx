9b6d083c7b41e09163db17d58562cf9ababd845f
dusanlecic <dusan.lecic5@gmail.com>
2024-02-03 21:00:30 +0100

    #10 update: temp import for testing

--- orbyx/tinywiki/setup.py
+++ orbyx/tinywiki/setup.py
@@ -6,9 +6,13 @@ setup(
     entry_points={
         'orbyx_data_source_plugin':
             ['template=tinywiki.engine:WikipediaDataSource'],
+        'orbyx_tinywiki':
+            ['template=tinywiki.engine:WikipediaDataSource'],
     },
     install_requires=[
         'orbyx-api==0.1',
+        'beautifulsoup4',
+        'requests'
     ],
     zip_safe=True
 )
