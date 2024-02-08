a530d17a371756fd59f858657dca4963ef0df826
dusanlecic <dusan.lecic5@gmail.com>
2024-02-08 21:48:53 +0100

    #22 update: new urls

--- orbyx/graph_explorer/orbyx/urls.py
+++ orbyx/graph_explorer/orbyx/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.load_graph_from_plugin, name='index')
+    path('', views.index, name='index'),
+    path('get-form-fields/', views.get_form_fields, name='get_form_fields'),
+    path('load-graph', views.load_graph, name='load_graph'), 
 ]
+
