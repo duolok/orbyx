e2904074ee047f0882140b4ad80adb2398b605e6
dusanlecic <dusan.lecic5@gmail.com>
2024-02-10 00:24:07 +0100

    #5 fix: missing method for getting dependencies

--- orbyx/api/services/data_source_api.py
+++ orbyx/api/services/data_source_api.py
@@ -2,9 +2,14 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, List
 from model.graph.graph import Graph
 class DataSourceAPI(ABC):
-    # @abstractmethod
+    @abstractmethod
     def get_name(self) -> str:
         """Abstract property for the name of the data source"""
+        pass
+ 
+    @abstractmethod
+    def get_requirements(self) -> List:         
+        """Abstract method for the name that will return list of dependencies necessary."""
         pass
 
     @abstractmethod
