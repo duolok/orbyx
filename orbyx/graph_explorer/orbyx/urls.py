79513fb3ed6c3540552ad6559c909fadad16b92e
dusanlecic <dusan.lecic5@gmail.com>
2024-02-09 02:03:32 +0100

    #22 fix: unused url

--- orbyx/graph_explorer/orbyx/urls.py
+++ orbyx/graph_explorer/orbyx/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('get-form-fields/', views.get_form_fields, name='get_form_fields'),
     path('load-graph', views.load_graph, name='load_graph'), 
-    path('', views.load_graph_from_plugin, name='index'),
     path('json', views.tree_view_data, name='json'),
     path('refresh', views.filtered_view, name='refresh'),
     path('search/<str:term>', views.search, name='search'),
